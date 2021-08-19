b8c71fcac2f24da9809ff709b731781bc2528a26
zerowu49 <jonathanputra67@gmail.com>
2021-08-19 13:44:13 +0700

    finish: latihan

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { IonApp, IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import {useRef, useState} from 'react'
+import { calculatorOutline, refreshOutline,   } from 'ionicons/icons';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,19 +22,98 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [calculatedBMI, setCalculatedBMI] = useState<number>()
+  const [statusBMI, setStatusBMI] = useState<String>("")  
+
+  const heightInputRef = useRef<HTMLIonInputElement>(null)
+  const weightInputRef = useRef<HTMLIonInputElement>(null)
+
+  const calculateBMI = () => {
+    const enteredWeight = weightInputRef.current!.value
+    const enteredHeight = heightInputRef.current!.value
+
+    if(!enteredWeight || !enteredHeight) return
+
+    const bmi = +enteredWeight / ((+enteredHeight/100)*(+enteredHeight/100) )
+    // console.log(bmi)
+    setCalculatedBMI(bmi)
+
+    if (bmi <= 8.5) {
+      setStatusBMI("Kurus")
+    } else if (bmi > 8.5 && bmi <= 24.9) {
+      setStatusBMI("Normal");
+    } else if (bmi > 24.9 && bmi <= 29.9) {
+      setStatusBMI("Gemuk");
+    } else {
+      setStatusBMI("Obesitas");
+    }
+  }
+
+  const resetInputs = () => {
+    weightInputRef.current!.value = ''
+    heightInputRef.current!.value = ''
+    setCalculatedBMI(0)
+  }
+
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>BMI Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding'>
+        <IonGrid className='ion-text-center'>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">
+                  Tinggi Badan (cm)
+                </IonLabel>
+                <IonInput ref={heightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">
+                  Berat Badan (kg)
+                </IonLabel>
+                <IonInput ref={weightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol size="6">
+              <IonButton onClick={calculateBMI}>
+                <IonIcon slot="start" icon={calculatorOutline}></IonIcon>
+                Calculate
+              </IonButton>
+            </IonCol>
+            <IonCol size="6">
+              <IonButton onClick={resetInputs}>
+                <IonIcon slot="start" name={refreshOutline}></IonIcon>
+                Reset
+              </IonButton>
+            </IonCol>
+          </IonRow>
+            {calculatedBMI && statusBMI && (
+              <IonRow>
+                <IonCol>
+                  <IonCard>
+                    <IonCardContent className='ion-text-center'>
+                      <h3>{calculatedBMI}</h3>
+                      <h1>{statusBMI}</h1>
+                    </IonCardContent>
+                  </IonCard>
+                </IonCol>
+              </IonRow>
+            )}
+        </IonGrid>
+      </IonContent>
+    </IonApp>
+  )};
 
 export default App;
