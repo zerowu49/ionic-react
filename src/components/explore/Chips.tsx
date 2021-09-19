003c2b662326cf20c6efd6f872b6ab30ea6c35a1
zerowu49 <jonathanputra67@gmail.com>
2021-09-19 19:17:15 +0700

    fix: optimize color

--- src/components/explore/Chips.tsx
+++ src/components/explore/Chips.tsx
@@ -6,19 +6,19 @@ import './Chips.css';
 const Chips: React.FC = () => {
   return (
     <IonCol id="carousel">
-      <IonChip color="light">
+      <IonChip >
         <IonIcon icon={bedOutline}/>
         <IonLabel>Hotels</IonLabel>
       </IonChip>
-      <IonChip color="light">
+      <IonChip>
         <IonIcon icon={ticketOutline} />
         <IonLabel>Things to Do</IonLabel>
       </IonChip>
-      <IonChip color="light">
+      <IonChip>
         <IonIcon icon={restaurantOutline} />
         <IonLabel>Restaurant</IonLabel>
       </IonChip>
-      <IonChip color="light">
+      <IonChip>
         <IonIcon icon={chatbubblesOutline} />
         <IonLabel>Forum</IonLabel>
       </IonChip>
